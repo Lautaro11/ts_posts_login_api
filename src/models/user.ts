2c22637e9faaf2a812a9e17c1203fcc84bf64b0d
Lautaro <robles_lautaro@hotmail.com.ar>
2022-08-28 18:01:07 +0200

    user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,1 +1,14 @@
-//probando 123
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowecase: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true },
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  },
+  { timestamps: true }
+);
+
+export default model("User", UserSchema);
